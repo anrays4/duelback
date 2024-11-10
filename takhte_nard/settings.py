1ac2c33d5eb3847483cf16761c54d215877e6344
3irna <arya.anrays.20@gmail.com>
2024-11-10 03:57:13 +0330

    debug false

--- takhte_nard/settings.py
+++ takhte_nard/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0cwq=sfzwll6g8e^rj#lr!lhs$&rf9$-)_vx&93z4yifrg)xtm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['duelback.com', 'www.duelback.com', "localhost"]
 
