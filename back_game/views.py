1e54745fcf5e8457aff6908d5474b59d6df2ba98
3irna <arya.anrays.20@gmail.com>
2024-11-22 22:55:49 +0330

    last debug

--- back_game/views.py
+++ back_game/views.py
@@ -55,6 +55,11 @@ class FindMatch(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def get_avatar_url(self, user):
+        if user.avatar and hasattr(user.avatar, 'url'):
+            return user.avatar.url
+        return '/statics/images/profile-default.jpg'  # مسیر آواتار پیش‌فرض
+
     def post(self, request):
         my_user = get_object_or_404(User, username=request.user)
 
@@ -83,7 +88,7 @@ class FindMatch(APIView):
                                                     player_2=player_2,
                                                     game_polling_time=poll_time, game_start_time=int(time.time()))
                 enemy_name = player_2.name
-                enemy_avatar = player_2.avatar.url
+                enemy_avatar = self.get_avatar_url(player_2)
                 enemy_level = player_2.level
                 wait_room_player_2.delete()
                 res = {"status": "go_to_play", "game_room_id": game_room.id, "enemy_name": enemy_name,
@@ -97,7 +102,7 @@ class FindMatch(APIView):
                 game_room = GameRoom.objects.get(player_1=my_user)
                 game_rome_id = game_room.id
                 enemy_name = game_room.player_2.name
-                enemy_avatar = game_room.player_2.avatar.url
+                enemy_avatar = self.get_avatar_url(game_room.player_2)
                 enemy_level = game_room.player_2.level
                 res = {"status": "your_match_ready", "game_room_id": game_rome_id, "enemy_name": enemy_name,
                        "enemy_avatar": enemy_avatar, "enemy_level": enemy_level}
@@ -107,7 +112,7 @@ class FindMatch(APIView):
                 game_room = GameRoom.objects.get(player_2=my_user)
                 game_rome_id = game_room.id
                 enemy_name = game_room.player_1.name
-                enemy_avatar = game_room.player_1.avatar.url
+                enemy_avatar = self.get_avatar_url(game_room.player_1)
                 enemy_level = game_room.player_1.level
                 res = {"status": "your_match_ready", "game_room_id": game_rome_id, "enemy_name": enemy_name,
                        "enemy_avatar": enemy_avatar, "enemy_level": enemy_level}
